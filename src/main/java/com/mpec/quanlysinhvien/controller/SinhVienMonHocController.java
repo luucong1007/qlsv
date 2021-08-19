e5e9b23a98696e2f876aa41683b95f15e3e36e4f
luucong1007 <luucong1007@gmail.com>
2021-08-19 21:38:21 +0700

    sinh_vien_mon_hoc...

--- src/main/java/com/mpec/quanlysinhvien/controller/SinhVienMonHocController.java
+++ src/main/java/com/mpec/quanlysinhvien/controller/SinhVienMonHocController.java
@@ -32,6 +32,7 @@ public class SinhVienMonHocController {
         return ResponseEntity.ok(sinhVienMonHocs);
     }
 
+
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody SinhVienMonHoc sinhVienMonHoc,
     @RequestParam("sinh_vien_id") int sinhVienId,
@@ -49,6 +50,27 @@ public class SinhVienMonHocController {
                 }).orElse(ResponseEntity.ok("sinh vien id not found"));
     }
 
-
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestParam("sinh_vien_mon_hoc_id")int sinhVienMonHocId,
+                                    @RequestParam("sinh_vien_id")int sinhVienId,
+                                    @RequestParam("mon_hoc_id")int monHocId,
+                                    @RequestParam("ngay_tao")String ngayTao,
+                                    @RequestParam("xoa")boolean xoa){
+        return sinhVienMonHocService.findByID(sinhVienMonHocId)
+                .map(sinhVienMonHoc -> {
+                    return sinhVienService.findById(sinhVienId, xoa)
+                            .map(sinhVien -> {
+                                return monHocService.findById(monHocId, xoa)
+                                                .map(monHoc -> {
+                                                    sinhVienMonHoc.setMonHoc(monHoc);
+                                                    sinhVienMonHoc.setSinhVien(sinhVien);
+                                                    sinhVienMonHoc.setNgayTao(ngayTao);
+                                                    sinhVienMonHoc.setXoa(xoa);
+                                                    sinhVienMonHocService.save(sinhVienMonHoc);
+                                                    return ResponseEntity.ok("success");
+                            }).orElse(ResponseEntity.ok("mon hoc id not found"));
+                }).orElse(ResponseEntity.ok("sinh vien id not found"));
+        }).orElse(ResponseEntity.ok("sinh vien mon hoc id not found"));
+    }
 
 }
